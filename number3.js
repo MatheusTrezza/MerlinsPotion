cc08a64fa636c0c278f5901dbb1e683bb743a743
Matheus Trezza <treza26@gmail.com>
2019-09-10 15:09:28 -0300

    Commit Teste number3

---
+++ number3.js
@@ -0,0 +1,34 @@
+<!Doctype html>
+   
+<html>
+                
+    <head>
+                <meta charset="utf-8">
+                <meta name="description" content="Treino">
+    </head>
+    
+    
+    
+    <body> 
+            <h1>Logica de programação e javascript</h1>
+        <script>
+        
+            var nome = prompt("Qual seu nome?");
+            alert("Bem vindo, " + nome);
+            
+            
+            var endereco = 1.2; // numero decimal // real
+            var estagravido = false; //boolean
+
+            
+        
+        
+        
+        </script>
+    
+    
+            </body>
+            <footer></footer>
+    
+    
+</html>
